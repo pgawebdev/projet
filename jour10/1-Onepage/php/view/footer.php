31fbf7adeebb6b911509b34c40fa52a3433a5b73
pgawebdev <pg.archen.webdev@gmail.com>
2020-02-13 10:56:04 +0100

    correction découpage

--- jour10/1-Onepage/php/view/footer.php
+++ jour10/1-Onepage/php/view/footer.php
@@ -1,7 +1,7 @@
     </main>
 
     <!--Sidebar droite-->
-    <?php require_once 'php/view/right-sidebar.php'?>
+    <?php require_once'php/view/right-sidebar.php'?>
 <!--Sidebar droite-->
 </div>
 <!-- Container global-->
---
+++ jour10/1-Onepage/php/view/right-sidebar.php
@@ -0,0 +1,8 @@
+ <!--Sidebar droite-->
+ <section id="right-side">
+            <img id="biche" src="assets/img/ufolk-folk-biche.jpg" alt="illustration d'une tête de biche">
+            <div id="button-container">
+                <button id="job"><i class="material-icons">arrow-upward><a href="./index.html"></a></button></i>
+            </div>
+        </section>
+        <!--Sidebar droite-->
