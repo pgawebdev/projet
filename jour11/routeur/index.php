92be7da74580f856586f4ac592a814fb3381ce3a
pgawebdev <pg.archen.webdev@gmail.com>
2020-02-18 15:21:45 +0100

    redirection conditionel propre

--- jour11/routeur/index.php
+++ jour11/routeur/index.php
@@ -2,7 +2,11 @@
 
 if (!empty($_REQUEST['page'])) { // Si le nom de la 'page' est rempli //
     $page = strtolower($_REQUEST['page']).'.php'; // Défini la variable $page comme le nom de la page active en minuscule et ajoute l'extension.php
-    $templates = scandir('templates', 1); // Défini la variable $template comme listant usous forme de tableau indexé le répertoire pointé 'templates' et l'ordonne en croissant
+    $templates = scandir('templates', 1); // Défini la variable $template comme listant sous forme de tableau indexé le répertoire pointé 'templates' et l'ordonne en croissant.
+    $check = 0;
+} else if (empty($_REQUEST['page'])){ // Si le paramètre 'page' est vide//
+    $check = 1;//On crée la variable $check avec pour valeur 1
+    $templates = scandir('templates', 1);
 }
 
 
@@ -21,11 +25,14 @@ if (!empty($_REQUEST['page'])) { // Si le nom de la 'page' est rempli //
     <?php
         require_once 'php/view/nav.php';
 
-        if (in_array($page, $templates)) {
-            require_once "templates/{$page}";
-        } else {
-            require_once 'templates/404.php';
+        if ($check > 0){ //Si $check est supérieur à 0
+            require_once "templates/home.php"; //On appel le fichier home.php
         }
+         else if (in_array($page, $templates)) { //Sinon,  si $page est = à $templates
+            require_once "templates/{$page}"; // On charge le fichier corresprondant à $page
+            } else { // Sinon
+            require_once 'templates/404.php'; // On charge la page d'erreur 404
+            }
      ?>
 </body>
 </html>
